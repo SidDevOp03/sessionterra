a25172b6d667cafdb4710ef7560951963e5f0b18
SidDevOp03 <139860896+SidDevOp03@users.noreply.github.com>
2024-04-16 16:36:48 +0530

    Update main1.tf

--- Terra1/main1.tf
+++ Terra1/main1.tf
@@ -1,1 +1,23 @@
+terraform { 
+  required_providers { 
+    azurerm = { 
+    source = "hashicorp/azurerm" 
+    version = "3.70.0" 
 
+    } 
+
+  } 
+
+} 
+
+provider "azurerm" { 
+features {
+  
+}
+} 
+
+
+resource "azurerm_resource_group" "rgsid1" {
+  name =  "rgsid1"
+  location = "east us"
+}
